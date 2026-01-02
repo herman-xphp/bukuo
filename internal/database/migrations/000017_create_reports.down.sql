d11a3003097186af80af046bdbb30bdd13db6662
herman-xphp <hermanxphp@gmail.com>
2026-01-02 11:49:06 +0800

    feat(reports): add Report entities and migration
    - Add ReportTemplate for saving report configs
    - Add ReportSchedule for automated reporting
    - Add GeneratedReport for archiving results
    - Migration 000017

---
+++ internal/database/migrations/000017_create_reports.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_generated_reports_company;
+DROP INDEX IF EXISTS idx_report_templates_company;
+
+DROP TABLE IF EXISTS generated_reports;
+DROP TABLE IF EXISTS report_schedules;
+DROP TABLE IF EXISTS report_templates;
